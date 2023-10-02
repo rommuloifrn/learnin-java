ac2c4ac4d506c1dbd30dfb97350ec41f44bd606e
rommuloifrn <roomulo.cavalkant@outlook.com>
2023-10-02 10:49:24 -0300

    continuing exercise

--- src/exercises/S13E1.java
+++ src/exercises/S13E1.java
@@ -1,6 +1,7 @@
 package exercises;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import entities.Department;
 import entities.HourContract;
@@ -9,19 +10,21 @@ import enums.WorkerLevel;
 
 public class S13E1 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
 		Department contabilidade = new Department("contabilidade");
 		Worker zezin = new Worker("zezin", WorkerLevel.MID_LEVEL, 2400.00, contabilidade);
 		
-		zezin.addContract(new HourContract(LocalDate.now(), 24.80, 30));
-		zezin.addContract(new HourContract(LocalDate.now(), 1300.60, 60));
+		System.out.println("enter number of contracts:");
+		int n = sc.nextInt();
 		
-		System.out.printf("%s", zezin.getContracts().get(0));
-		System.out.printf("%s", zezin.getContracts().get(1));
-		
-		zezin.remove(zezin.getContracts().get(0));
-		System.out.println("remoção!");
-		
-		System.out.printf("%s", zezin.getContracts().get(0));
+		for (int i=1; i<=n; i++) {
+			LocalDate date = LocalDate.now();
+			System.out.println("enter the value per hour of the contract " + i + ":");
+			Double value = sc.nextDouble();
+			System.out.println("enter the hours of contract " + i + ":");
+			Integer hours = sc.nextInt();
+			zezin.addContract(new HourContract(date, value, hours));
+		}
 		
 		
 		
